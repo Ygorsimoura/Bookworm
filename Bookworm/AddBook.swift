b2a5e9dc6b38030b5dd1f25d0ba5f8f58f1d6de2
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-25 20:41:15 -0300

    feat: Creatting book file with book class with SwiftData and AddBookView

---
+++ Bookworm/AddBook.swift
@@ -0,0 +1,18 @@
+//
+//  AddBook.swift
+//  Bookworm
+//
+//  Created by Ygor Simoura on 25/11/23.
+//
+
+import SwiftUI
+
+struct AddBook: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBook()
+}
---
+++ Bookworm/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by Ygor Simoura on 25/11/23.
+//
+
+import Foundation
