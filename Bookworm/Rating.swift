933b18ae5d8c218b0274512b7b1190978ef82d5e
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-25 21:55:53 -0300

    feat: Create a custom star rating

---
+++ Bookworm/Rating.swift
@@ -0,0 +1,18 @@
+//
+//  Rating.swift
+//  Bookworm
+//
+//  Created by Ygor Simoura on 25/11/23.
+//
+
+import SwiftUI
+
+struct Rating: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rating()
+}
