5d239f8281299381d244f949ef9945125c9fdd85
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-26 21:49:05 -0300

    feat: creatting DeatilView

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Ygor Simoura on 26/11/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
